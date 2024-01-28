a0a6e82ed42fb5baa2debac47c217c0a4950e151
shafia31 <shafia.shafique@gmail.com>
2024-01-28 23:59:39 +0530

    added

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const searchIcon = document.querySelector('.search-icon');
 const searchForm = document.querySelector('.search-form');
+const menuIcon = document.querySelector('.menu-icon');
+const navbar = document.querySelector('.navbar');
 
 searchIcon.addEventListener('click', () => {
 searchForm.classList.add("active");
 cartItemsContainer.classList.remove("active");
+navbar.classList.remove("active");
 });
+
+menuIcon.addEventListener('click', () => {
+    navbar.classList.add("active");
+    searchForm.classList.remove("active");
+    cartItemsContainer.classList.remove("active");
+    });
 
 
 const cartIcon = document.querySelector('.cart-icon');
@@ -13,4 +22,5 @@ const cartItemsContainer = document.querySelector('.cart-items-container');
 cartIcon.addEventListener('click', () => {
     cartItemsContainer.classList.add("active");
     searchForm.classList.remove("active");
+    navbar.classList.remove("active");
 });
